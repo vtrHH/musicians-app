5a766bb69b858b6ca598d97f206bdfce86534bd3
Verena Traub <verena@yngage.de>
2021-02-09 21:27:06 +0100

    small changes

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,17 @@
 const express = require('express');
 const router = new express.Router();
 const routeGuard = require('./../middleware/route-guard');
-const uploadMiddleware = require('./../middleware/file-upload');
 
 const Offer = require('./../models/offer');
 
-router.get(
-  '/',
-  routeGuard,
-  uploadMiddleware.single('image'),
-  (req, res, next) => {
-    let image;
-    if (req.file) {
-      image = req.file.path;
-    }
-    Offer.find()
-      .then((offers) => {
-        res.render('home', { offers, image });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  }
-);
+router.get('/', (req, res, next) => {
+  Offer.find()
+    .then((offers) => {
+      res.render('home', { offers });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 module.exports = router;
