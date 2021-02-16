0c6d337d23d5b5abe273345400bf3b98aa3fb92b
Verena Traub <verena@yngage.de>
2021-02-16 21:10:16 +0100

    adds basic google maps

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,5 +1,5 @@
 function initMap() {
-  const mapElement = document.getElementById('location-map');
+  const mapElement = document.getElementById('map');
 
   let map = new google.maps.Map(mapElement, {
     center: { lat: 49.02961956691756, lng: 10.1014974899453 },
