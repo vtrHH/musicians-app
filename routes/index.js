2b39fec25a7fda0ded2bc1487a98b9313638060e
Verena Traub <verena@yngage.de>
2021-02-20 08:09:43 +0100

    adds text overflow, fixes home issue when being singned in

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,26 @@ const routeGuard = require('./../middleware/route-guard');
 const Offer = require('./../models/offer');
 
 router.get('/', (req, res, next) => {
-  res.render('landing-page');
+  const page = Number(req.query.page) || 1;
+
+  const limit = 6;
+  const skip = (page - 1) * limit;
+
+  Offer.find()
+    .skip(skip)
+    .limit(limit)
+    .populate('creator')
+    .sort({ creationDate: -1 })
+    .then((offers) => {
+      res.render('landing-page', {
+        offers,
+        previousPage: page - 1,
+        nextPage: offers.length ? page + 1 : 0
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 router.get('/home', routeGuard, (req, res, next) => {
